5594998d7dae07ba265e0f3f1301323d6f6e4c7c
VishalGupta707 <vishalgupta70777@gmail.com>
2026-01-31 00:06:20 +0530

    fixed code for deployment

--- Gateway/src/main/java/com/fitness/Gateway/SecurityConfig.java
+++ Gateway/src/main/java/com/fitness/Gateway/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.fitness.Gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -27,10 +28,14 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
+
+    @Value("${frontend_url}")
+    private String frontendUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(frontendUrl));
         config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization","Content-Type","X-User-ID"));
         config.setAllowCredentials(true);
